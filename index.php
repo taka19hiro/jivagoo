e3e263251bc7d9d81480454652c8f30a38cfdf7b
taka19hiro <65790731+taka19hiro@users.noreply.github.com>
2020-10-20 13:57:58 +0900

    php commit test

---
+++ index.php
@@ -0,0 +1,121 @@
+<?php
+//このスクリプトはおばけスキャン用のテストスクリプト
+//まずアプリからpostされていない場合の処理をします
+//接続元のIPは抜いておく
+$ip = @$_SERVER['REMOTE_ADDR'];
+//送られてくるだろうアプリ識別子
+$appcode = "202010161444jivagoo";
+//このスクリプトのパス
+$script_php = $_SERVER['SCRIPT_NAME'];
+
+//POSTされていない場合はその案内
+if ($_SERVER['REQUEST_METHOD']!='POST'){
+    //ローカルからの接続の場合
+    $iparray = explode(".", $ip);
+    if($iparray[0] == '192' && $iparray[1] == '168' && $iparray[2] == '128'){
+        //phpinfo();
+        session_start();
+        
+        echo '<html><head><meta http-equiv="Refresh" content="30"><title>GhostScan Server</title></head><center><body><h1>Server load status</h1><p>'.
+            date("Y-m-d H:i:s").' (<span id="timer">30</span>)</p><hr width="800">';
+        echo '
+			<script>
+			window.onload=function(){
+			    timer();
+			}
+
+			function timer(){
+			    setTimeout(function () {
+			        var time = document.getElementById("timer").innerText;
+			        time--;
+			        countdown(time);
+			    }, 1000);
+			}
+
+			function countdown(time){
+			    document.getElementById("timer").innerText = time;
+			    timer();
+			}
+			</script>';
+
+        //CPU使用率
+        $load = sys_getloadavg();
+        
+        //メモリの使用率
+        $free = Shell_exec('free');
+        $free = (string)trim($free);
+        $free_arr = explode("\n", $free);
+        $mem = explode(" ", $free_arr[1]);
+        $mem = array_filter($mem);
+        $mem = array_merge($mem);
+        $memory_usage = $mem[2]/$mem[1]*100;
+        //処理速度の簡易ベンチマーク
+        $time_start = microtime(true);
+        for($i=0;$i<100000000;$i++){};
+        $time = microtime(true) - $time_start;
+        
+        echo '<table border="0" width="800" bgcolor="#999999" cellpadding="6" cellspacing="1">
+            <tr><td width="50%" bgcolor="#ffddee"><p>CPU Usage</p></td><td width="50%" bgcolor="#ffffff"><p>'.$load[0].'％ ('.$_SESSION['cpu'].')</p></td></tr>
+            <tr><td width="50%" bgcolor="#ddddff"><p>Memory Usage</p></td><td width="50%" bgcolor="#ffffff"><p>'.round($memory_usage,2).'％ ('.$_SESSION['memory'].')</p></td></tr>
+            <tr><td width="50%" bgcolor="#ffddee"><p>ServerIP</p></td><td width="50%" bgcolor="#ffffff"><p>'.$_SERVER['SERVER_ADDR'].'</p></td></tr>
+            <tr><td width="50%" bgcolor="#ddddff"><p>DocumentRoot</p></td><td width="50%" bgcolor="#ffffff"><p>'.$_SERVER['DOCUMENT_ROOT'].'</p></td></tr>
+            <tr><td width="50%" bgcolor="#ffddee"><p>UserAgent</p></td><td width="50%" bgcolor="#ffffff"><p>'.$_SERVER['HTTP_USER_AGENT'].'</p></td></tr>
+            <tr><td width="50%" bgcolor="#ddddff"><p>ClientIP</p></td><td width="50%" bgcolor="#ffffff"><p>'.$_SERVER['REMOTE_ADDR'].'</p></td></tr>
+            <tr><td width="50%" bgcolor="#ffddee"><p>Benchmark of time to 100million counts</p></td><td width="50%" bgcolor="#ffffff"><p>'.round($time,2).' sec. ('.$_SESSION['benchi'].')</p></td></tr>
+            </table>';
+        print '<br><a href="./old_wspri">昔のWSPRI</a></center></body></html>';
+        
+		$_SESSION['cpu'] = $load[0].'％';
+		$_SESSION['memory'] = round($memory_usage,2).'％';
+		$_SESSION['benchi'] = round($time,2).' sec.';
+    }else{
+        echo '
+        <html><head><title>GhostScan Server</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        </head>
+        <body><h1>GhostScan GameServer</h1>
+        <p>This game server is for GhostScan only.</p>
+        <p style="color:red;">Only available from GhostScan, an Android game app.</p>
+        <a href="https://play.google.com/store/apps/details?id=jp.dip.wspri.jivagoo" target="_blank">
+        <img src="https://lh3.googleusercontent.com/gD5aQb8FWaQ73bwCEtgc_k8Ry-2bkM6WAaqtQmSYlKjyEGHgP0aOuT4ZzZYAX0-gyv0=s180-rw">
+        <p>GhostScan Download Google Play</P></a>
+        ';
+        print "<p>".$ip."</p>";
+        echo '
+        </body></html>
+        ';
+        //POSTされていないでアクセスしてきたらIPを記録するか検討
+    }
+}else{//POSTされている
+    //appcodeがPOSTされているなら正式なログイン
+    if($_POST['appcode']==$appcode){
+        //終了コードが送られてこない場合は端末のゲームシステムに依存し、サーバの処理を終了する
+        //初めてのログインならばfalseを返して端末の初期値を端末で生成し、送り、アカウントを登録してDBを生成
+
+        //検索してアカウントがあればそれを読み込む
+
+        //終了コードが送られてきた場合：勝手に冒険が始まりログインするまでサーバのゲームシステムに依存
+        //端末のGPSの移動距離を元にサーバMapを移動してイベントを発生させる
+
+    }else{//appcodeがPOSTされていないなら不正ログイン
+        echo '
+        <html><head><title>GostScan Server</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        </head>
+        <body><h1>GhostScan GameServer</h1>
+        <p>This game server is for GhostScan only.</p>
+        <p style="color:red;">Only available from GhostScan, an Android game app.</p>
+        <p style="color:red;">It is determined that the POST data sent is not from a ghost scan.</p>
+        <p style="color:red;">If you install and start Ghost Scan, you will be automatically logged in to this game server.</p>
+        <a href="https://play.google.com/store/apps/details?id=jp.dip.wspri.jivagoo" target="_blank">
+        <img src="https://lh3.googleusercontent.com/gD5aQb8FWaQ73bwCEtgc_k8Ry-2bkM6WAaqtQmSYlKjyEGHgP0aOuT4ZzZYAX0-gyv0=s180-rw">
+        <p>GhostScan Download Google Play</P></a>
+        ';
+        print $ip;
+        echo '
+        </body></html>
+        ';
+        //POSTされてて不正アクセスならIPを残すか検討する
+    }
+}
+?>
